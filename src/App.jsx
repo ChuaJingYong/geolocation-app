4712a799667057fe295b9798d1c72094ed15a65d
Jing Yong <jyongcode@gmail.com>
2024-05-02 08:43:34 +0800

    Fixed typo and added dependency array

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useEffect } from 'react';
 import { useState } from 'react'
 
 function App(){
-  const [geoLocation, setGeoLocation] = useState({ latitude:0, longtigude:0})
+  const [geoLocation, setGeoLocation] = useState({ latitude:0, longitude:0})
 
   function fetchGeoLocation(callback){
     navigator.geolocation.getCurrentPosition((position) => {
         const data = { latitude:position.coords.latitude, 
-                longtigude:position.coords.longitude
+                longitude:position.coords.longitude
               };
         callback(data)
-      });
+      }, (error)=>{
+        console.error("Error fetching geolocation",error)
+      }
+    );
   }
 
   console.log('geolcation',geoLocation)
@@ -24,14 +27,14 @@ function App(){
     },1000) //refreshes geolocation every 1s
 
     return ()=>{clearInterval(intervalId)}
-  })
+  }, [])
   
 
   return(
     <>
       <h1>GeoTrack App</h1>
       <h3>A simple app to test and get geo location</h3>
-      <p><strong>Latitude:</strong> {geoLocation.latitude}, <strong>Longtigude:</strong> {geoLocation.longtigude}</p>
+      <p><strong>Latitude:</strong> {geoLocation.latitude}, <strong>Longtigude:</strong> {geoLocation.longitude}</p>
     </>
   )
 }
